bafcf6da53e1259549b81d439523881f0b8dbb04
DiegoDamianB <76851925@certus.edu.pe>
2021-07-03 22:56:31 -0500

    nuevo repo

---
+++ javascript/darkmode.js
@@ -0,0 +1,13 @@
+//IMPLEMENTAR DARKMODE
+function activarDarkMode() {
+    //Body referencia (background color)
+    const fondoWeb = document.body;
+    const mainContenedor = document.querySelector('main.contenedor'); //Referencia a un <main class='contenedor'>...</main>
+
+    fondoWeb.classList.toggle('dark-mode');
+    mainContenedor.classList.toggle('dark-mode');
+}
+
+//Referencia al elemento
+const botonDarkMode = document.querySelector('.boton-dark-mode');
+botonDarkMode.addEventListener('click', activarDarkMode);
